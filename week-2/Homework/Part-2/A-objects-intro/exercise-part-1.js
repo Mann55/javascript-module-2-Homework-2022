9f876ef985922a32b37a8733b27311b99cf55c4b
manpreet <manhspman5@gmail.com>
2022-09-24 14:07:00 +0200

    part2 of first week is done

--- week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
+++ week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
@@ -21,6 +21,44 @@ let bicycle = {
   brand: "Assembled by Ali Express",
   model: "Unknown",
   color: "blue",
-  horsepower: 115,
-  doors: 5,
+  brakes: "hydraulics",
+  electric: true,
 };
+
+let mobile = {
+  brand: "One Plus",
+  model: "nord 2",
+  color: "blue",
+  memory: "8 gb",
+  doubleCamera: true,
+};
+
+let watch = {
+  brand: "Apple",
+  model: "Series 8",
+  color: "black",
+  bluetooth: true,
+  touchScreen: true,
+};
+
+let table = {
+  color: "white",
+  legs: 4,
+  material: "wooden",
+  large: false,
+  small: true,
+};
+
+const room = {
+  color: "while",
+  size: "medium",
+  status: "rented",
+  furnished: true,
+  decorated: false,
+};
+
+console.log(bicycle);
+console.log(mobile);
+console.log(watch);
+console.log(table);
+console.log(room);
